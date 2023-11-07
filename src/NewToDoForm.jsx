fec50cc17fb36d4f8b3dcd373ee4875e7f109317
wnf <william@fluxrme.com>
2023-11-07 00:55:29 +0000

    testing same functionality but using a <form>

--- src/NewToDoForm.jsx
+++ src/NewToDoForm.jsx
@@ -3,20 +3,25 @@ import { useState } from "react";
 export default function NewToDoForm({ addToDo }) {
   const [newToDo, setNewToDo] = useState("");
 
-  function handleAddToDo() {
-    addToDo(newToDo);
-    setNewToDo("");
-  }
+function handleAddToDo(evt) {
+  evt.preventDefault();
+  addToDo(newToDo);
+  setNewToDo("");
+}
 
   return (
     <>
       <h2>New To-Do</h2>
-      <input
-        value={newToDo}
-        onChange={(evt) => setNewToDo(evt.target.value)}
-        placeholder="New To-Do"
-      />
-      <button onClick={handleAddToDo}>ADD TO-DO</button>
+      <form onSubmit={handleAddToDo}>
+        <input
+          value={newToDo}
+          onChange={(evt) => setNewToDo(evt.target.value)}
+          placeholder="To-Do"
+          required 
+          pattern=".{4,}"
+        />
+        <button type="submit">ADD TO-DO</button>
+      </form>
     </>
   );
 }
